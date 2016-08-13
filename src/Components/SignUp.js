00d0f1b2bc9ee128458a3610725526fd6c193630
shreiya chowdhary <shreiya.c@gmail.com>
2016-08-13 17:19:38 -0400

    getting the uid

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -33,13 +33,13 @@ render() {
     <div>
 
       <h1>SIGN UP</h1>
-        <ol>
+        <ul>
           <li> First Name: <input onChange={(event) => this.setState({firstName: event.target.value})} /></li>
           <li> Last Name: <input onChange={(event) => this.setState({lastName: event.target.value})} /></li>
           <li> Email: <input onChange={(event) => this.setState({email: event.target.value})} /></li>
           <li> Username: <input onChange={(event) => this.setState({username: event.target.value})} /></li>
           <li> Password: <input onChange={(event) => this.setState({pw: event.target.value})} /></li>
-        </ol>
+        </ul>
         <button onClick={(event) => this.makeUser(event)}><Link to="/LogIn">Make Me!</Link></button>
       <h1>OR</h1>
         <h1><Link to="/LogIn">SIGN IN</Link></h1>
