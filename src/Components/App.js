9a65592c073275c1adfd1fed7aeffaad62094d35
shreiya chowdhary <shreiya.c@gmail.com>
2016-08-14 16:22:23 -0400

    changes to display-stuff

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import NavLink from './NavLink';
-
-import { Nav } from 'react-bootstrap';
+import 'react-bootstrap';
 
 
 
@@ -14,11 +13,12 @@ class App extends Component {
 
         <h2 className="Header">BOMBERMAN</h2>
           <ul className="nav nav-tabs">
-            <li role="presentation" className="active" className="home"><NavLink to='/' onlyActiveOnIndex>Home</NavLink></li>
+            <li data-toggle="tooltip" data-placement="left" title="Tooltip on left" role="presentation" className="active" className="home"><NavLink to='/' onlyActiveOnIndex>Home</NavLink></li>
             <li role="presentation" ><NavLink to='/about_game'>About The Game</NavLink></li>
             <li role="presentation"><NavLink to='/about_makers'>About The Makers</NavLink></li>
             <li role="presentation"><NavLink to="/SignUp">SIGN UP AND PLAY</NavLink></li>
           </ul>
+
           {this.props.children}
       </div>
       );
